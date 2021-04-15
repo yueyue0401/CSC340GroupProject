2dbaa6057cb8e279055dddade22174ea2d8fe5f7
Alan Chuang <cchuang2@mail.sfsu.edu>
2021-04-15 11:41:35 +0800

    some fake data

--- main.cpp
+++ main.cpp
@@ -13,7 +13,22 @@ using namespace std;
 int main() {
     std::cout << "Welcome to Group 15 project" << std::endl;
 
-    vector<Movie> movies; // Tommy reading file into this object
+    std::vector <Movie> movies;
+    std::vector<std::string> actors(1, "Amy");
+    std::vector<std::string> actGenres(1, "act");
+    std::vector<std::string> docGenres(1, "doc");
+    Movie m1("Avatar", "Apple", "John", 100, actors, actGenres);
+    Movie m2("Iron man", "Marvel", "Alan", 200, actors, actGenres);
+    Movie m3("Superman", "Marvel", "Roger", 100, actors, actGenres);
+    Movie m4("La la land", "Apple", "Luke", 100, actors, docGenres);
+    Movie m5("The earth", "Discovery", "Amy", 345, actors, docGenres);
+    Movie m6("Ocean", "Discovery", "Lisa", 134534500, actors, actGenres);
+    movies.push_back(m1);
+    movies.push_back(m2);
+    movies.push_back(m3);
+    movies.push_back(m4);
+    movies.push_back(m5);
+    movies.push_back(m6);
 
     string genre;
 
@@ -29,8 +44,8 @@ int main() {
         ActorDirectorProcessor adp;
         adp.process(movies, genres);
 
-        ActorsProfitProcessor app;
-        app.process(movies, genres);
+//        ActorsProfitProcessor app;
+//        app.process(movies, genres);
 
         // Kobe processor
     }
